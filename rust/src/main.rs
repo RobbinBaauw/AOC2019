fc80acbb8eada4e10445a87d8159c7713dcf8be1
Robbin Baauw <robbin.baauw@gmail.com>
2019-12-04 16:23:26 +0100

    Initialized rust project

---
+++ rust/src/main.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+#![allow(clippy::unreadable_literal)]
+
+use std::error::Error;
+
+mod day5;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
